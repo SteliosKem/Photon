b0b55e69336dbd8e2224a2f3428055823ac74496
SteliosKem <stylianoskem@gmail.com>
2026-07-14 16:08:19 -0700

    Added GLM

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <glm/glm.hpp>
 
 int main() {
+  glm::vec3 origin = glm::vec3(0.0f, 0.0f, 0.0f);
   const int IMAGE_WIDTH = 256;
   const int IMAGE_HEIGHT = 256;
 
   std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
 
   for (int j = 0; j < IMAGE_HEIGHT; j++) {
+    std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
+
     for (int i = 0; i < IMAGE_WIDTH; i++) {
       auto r = double(i) / (IMAGE_WIDTH - 1);
       auto g = double(j) / (IMAGE_HEIGHT - 1);
@@ -19,6 +23,6 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
-  
+  std::clog << "\rDone.                 \n";
   return 0;
 }
